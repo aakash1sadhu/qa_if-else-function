be14802e3cd4c58b1ffd99b514ba33ac1e9a956d
Aakash Sadhu <aakashsadhu@hotmail.com>
2026-05-02 06:04:03 +0100

    fixes

--- src/ifElse.test.js
+++ src/ifElse.test.js
@@ -1,9 +1,7 @@
 'use strict';
 
-const { ifElse } = require('./ifElse');
-
 describe('ifElse', () => {
-  onst { ifElse } = require('./ifElse');
+  const { ifElse } = require('./ifElse');
 
   it('should be declared', () => {
     expect(ifElse).toBeInstanceOf(Function);
@@ -12,26 +10,28 @@ describe('ifElse', () => {
   it('should call second if condition is false', () => {
     let result = 0;
     const condition = () => Math.PI < 0.4;
-    const first = () => result = 1;
-    const second = () => result = 2;
+    const first = () => (result = 1);
+    const second = () => (result = 2);
+
     ifElse(condition, first, second);
     expect(result).toBe(2);
-  })
+  });
 
   it('should call first if condition is true', () => {
     let result = 0;
     const condition = () => Math.PI > 0.5;
-    const first = () => result = 1;
-    const second = () => result = 2;
-    ifElse(condition, first, second)
-    expect(result).toBe(1)
+    const first = () => (result = 1);
+    const second = () => (result = 2);
+
+    ifElse(condition, first, second);
+    expect(result).toBe(1);
   });
 
   it('should not return anything', () => {
-    const condition = () =>  true;
+    const condition = () => true;
     const first = () => true;
     const second = () => true;
 
     expect(ifElse(condition, first, second)).toBeUndefined();
-  })
+  });
 });
